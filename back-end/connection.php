e1112b33785e9ecdb00078326a7b3ae0defd1c8e
Samuel_Moo <samuelmoo05@gmail.com>
2022-11-18 17:50:21 -0600

    quick fix to the connection still not done

--- back-end/connection.php
+++ back-end/connection.php
@@ -1,12 +1,48 @@
 <?php
-    //declarar variables 
+    //Variables 
     $server='localhost';
-    $db='Prior_php_tests_2';
     $user='root';
-    $pwd='';
-
+    $password='';
+    $db='Prior_php_tests_2';
+    
     //conexion
-    $DB= new PDO("mysql:host= $server; dbname=$db", $user, $pwd);
-    $DB -> exec("SET CHARACTER SET utf8");
+    try{
+        $conn=new PDO("mysql:host=$server;dbname=$db", $user, $password);
+        
+        //conf variables en caso de errores
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        //traer variables
+        $name=$_POST["name-sign"];
+        $email=$_POST["email-sign"];        
+        $pass_sign=$_POST["pwd-sign"];
+        
+        //cifrar password
+        $password_cifrada=password_hash($password, PASSWORD_DEFAULT);
+        
+        //Insert en la tabla
+        $query=$conn->prepare("INSERT INTO user(name_, email, password_) VALUES (?,?,?)");
+        
+        //Datos
+        $query->bindParam(1, $name, PDO::PARAM_STR, 255);
+        $query->bindParam(2, $email, PDO::PARAM_STR, 255);
+        $query->bindParam(3, $password_cifrada, PDO::PARAM_STR, 255);
 
+        //ejecutar
+        $query->execute();
+
+        //cerrar conexion
+        $conn=null;
+
+        //confirmar a usuario
+        echo "Felicidades shinji";
+
+        //redireccion
+        header('location:../html/login.html');
+
+    }
+    catch(PDOException $err){
+        //mandar error
+        echo "Su conexion ha fallado por ".$err->getMessage();
+    }
 ?>
