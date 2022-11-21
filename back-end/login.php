604172909e2436003d2d947fe59970e009807c50
Samuel_Moo <samuelmoo05@gmail.com>
2022-11-20 21:41:52 -0600

    login pirateado v1

--- back-end/login.php
+++ back-end/login.php
@@ -11,25 +11,95 @@
         
         //conf variables en caso de errores
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              
+        // Encrypt cookie
+        function encryptCookie( $userid ) {
         
-        //traer variables
-        $email_login=$_POST["email"];
-        $password_login=$_POST["password"];
-
-        //cifrar password para comparar despues
-        $password_cifrada=password_hash($password_login, PASSWORD_DEFAULT);
-
-        //seleccionar datos de la tabla
-        $query->prepare("SELECT * FROM users WHERE user_email=$email_login AND user_password=$password_cifrada");
-
-        $query_result = mysql_query($query);
+            $key = hex2bin(openssl_random_pseudo_bytes(4));
         
-        //test
-        $query->execute();
-        echo $query;
+            $cipher = "aes-256-cbc";
+            $ivlen = openssl_cipher_iv_length($cipher);
+            $iv = openssl_random_pseudo_bytes($ivlen);
         
-        //checar que se 
+            $ciphertext = openssl_encrypt($userid, $cipher, $key, 0, $iv);
         
+        
+            return( base64_encode($ciphertext . '::' . $iv.'::'.$key) );
+        }
+        
+        // Decrypt cookie
+        function decryptCookie( $ciphertext ) {
+        
+            $cipher = "aes-256-cbc";
+        
+            list($encrypted_data, $iv,$key) = explode('::', base64_decode($ciphertext));
+            return openssl_decrypt($encrypted_data, $cipher, $key, 0, $iv);
+        
+        }
+        
+        
+        // Check if $_SESSION or $_COOKIE already set
+        if( isset($_SESSION['userid']) ){
+            header('Location: home.php');
+            exit;
+        }else if( isset($_COOKIE['rememberme'] )){
+        
+            // Decrypt cookie variable value
+            $userid = decryptCookie($_COOKIE['rememberme']);
+        
+            // Fetch records
+            $stmt = $conn->prepare("SELECT count(*) as cntUser FROM users WHERE id=:id");
+            $stmt->bindValue(':id', (int)$userid, PDO::PARAM_INT);
+            $stmt->execute(); 
+            $count = $stmt->fetchColumn();
+        
+            if( $count > 0 ){
+                $_SESSION['userid'] = $userid; 
+                header('Location: main.php');
+                exit;
+            }
+        }
+        
+        // On submit
+        if(isset($_POST['but_submit'])){
+        
+            $username = $_POST['txt_uname'];
+            $password = $_POST['txt_pwd'];
+        
+            if ($username != "" && $password != ""){
+               
+            // Fetch records
+            $stmt = $conn->prepare("SELECT count(*) as cntUser,id FROM usuarios WHERE username=:username and password=:password ");
+            $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+            $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+            $stmt->execute(); 
+            $record = $stmt->fetch(); 
+        
+            $count = $record['cntUser'];
+        
+            if($count > 0){
+                $userid = $record['id'];
+        
+                if( isset($_POST['rememberme']) ){
+        
+                    // Set cookie variables
+                    $days = 30;
+                    $value = encryptCookie($userid);
+        
+                    setcookie ("rememberme",$value,time()+ ($days * 24 * 60 * 60 * 1000));
+        
+                }
+        
+                $_SESSION['userid'] = $userid; 
+                header('Location: main.php');
+                exit;
+            }else{
+                echo "Invalid username and password";
+            }
+        
+            }   
+        
+        } 
 
     }
     catch(PDOException $err){
