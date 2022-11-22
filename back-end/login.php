bef4f9d7731a3b85e540a1c9ff8ffca2d6dc919a
Samuel_Moo <samuelmoo05@gmail.com>
2022-11-21 20:48:50 -0600

    LOGIN PIRATEADO PERO AHORA SE VE MAS BONITO

--- back-end/login.php
+++ back-end/login.php
@@ -9,81 +9,63 @@
     try{
         $conn=new PDO("mysql:host=$server; dbname=$db", $user, $password);
         
-        //conf variables en caso de errores
+        //conf variables en caso de erroreS
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              
+        
+        //Crear una nueva sesion
         session_start(); 
 
+
         if (isset($_POST['uname']) && isset($_POST['password'])) {
-
             function validate($data){
-
             $data = trim($data);
-
             $data = stripslashes($data);
-
             $data = htmlspecialchars($data);
-
             return $data;
-
         }
 
         $uname = validate($_POST['uname']);
-
         $pass = validate($_POST['password']);
-
+        
         if (empty($uname)) {
-
             header("Location: index.php?error=User Name is required");
-
             exit();
+        }
 
-        }else if(empty($pass)){
-
+        else if(empty($pass)){
             header("Location: index.php?error=Password is required");
-
             exit();
+        }
 
-        }else{
-
+        else{
             $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
-
             $result = mysqli_query($conn, $sql);
-
+            
             if (mysqli_num_rows($result) === 1) {
-
+                
                 $row = mysqli_fetch_assoc($result);
-
+                
                 if ($row['user_name'] === $uname && $row['password'] === $pass) {
-
                     echo "Logged in!";
-
                     $_SESSION['user_name'] = $row['user_name'];
-
                     $_SESSION['name'] = $row['name'];
-
                     $_SESSION['id'] = $row['id'];
-
                     header("Location: home.php");
-
                     exit();
-
-                }else{
-
-                    header("Location: index.php?error=Incorect User name or password");
-
-                    exit();
-
+                
                 }
-
-            }else{
-
+                else{
+                    header("Location: index.php?error=Incorect User name or password");
+                    exit();
+                }   
+            }
+            else{
                 header("Location: index.php?error=Incorect User name or password");
                 exit();
-
             }
         }
-    }else{
+    }
+    else{
         header("Location: index.php");
         exit();
     }
