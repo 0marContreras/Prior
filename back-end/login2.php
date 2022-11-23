5686f0d4676229061752a10b65f32fd8ae984b82
Samuel_Moo <samuelmoo05@gmail.com>
2022-11-22 18:57:54 -0600

    .

--- back-end/login2.php
+++ back-end/login2.php
@@ -13,29 +13,35 @@
      //conf variables en caso de erroreS
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    //Traer variables de el front
      $email_login=$_POST["email"];
      $pass_login=$_POST["password"];
      $pass_login_encrypted=password_hash($pass_login, PASSWORD_DEFAULT);
     
-     
+    //Iniciamos una session
     session_start();
     
+    //Variables de Session
     $_SESSION['email']=$email_login;
 
+    //query pa seleccionar email y password 
     $query="SELECT COUNT(*) FROM users WHERE user_email='$email_login'";
     //AND user_password='$pass_login_encrypted '
     
+    //ejecutamos query
     $query_result=$conn->query($query);
 
+    //contamos cuantas columnas nos da el query
     $count=$query_result->fetchColumn();
 
+    //Si nos regresa una columa pa adelante caminante, si no pa atrás papa de regreso al login 
     if ($count === 1) {
         echo "Felicidades shinji";
     }    
     else{
         echo "Subete al EVA shinji";
     }
-    }
+    }//Catch de toda la vida que nos manda error si valió todo 
     catch(PDOException $err){
         //mandar error
         echo "Error: ".$err->getMessage();
