b4bcd594a20fdbcaaaff50a656f7710858c3f6e4
Samuel_Moo <samuelmoo05@gmail.com>
2022-11-23 23:43:22 -0600

    Creacion of select_test

---
+++ back-end/select_test.php
@@ -0,0 +1,30 @@
+<?php
+
+//Variables 
+$server='localhost';
+$user='root';
+$password='';
+$db='Prior_php_tests_2';
+$errmsg="";
+
+//conexion
+try{
+    $conn=new PDO("mysql:host=$server; dbname=$db", $user, $password);
+    
+    //conf variables en caso de erroreS
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+    
+    
+    
+    }
+    //Catch de toda la vida que nos manda error si valió todo 
+    catch(PDOException $err){
+        //mandar error
+        echo "Error: ".$err->getMessage();
+    }
+
+
+
+?>
+     
